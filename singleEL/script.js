57674bbfa5120b98aaf51848327f87a707706d3e
mikmail <mikael@rund.no>
2021-08-17 15:52:16 +0200

    Delt opp i mapper og lagt til flere html filer

--- singleEL/script.js
+++ singleEL/script.js
@@ -1,6 +1,5 @@
 const bracketTemplate = document.querySelector("#bracket-template")
 const bracketContainer = document.getElementById("tree")
-let bracketHeight = 150;
 let playerCount = 8;
 let colCount = Math.floor(Math.log(playerCount)/Math.log(2))
 bracketContainer.style.gridTemplateColumns = `repeat(${colCount}, min-content)`
@@ -8,15 +7,17 @@ bracketContainer.style.gridTemplateColumns = `repeat(${colCount}, min-content)`
 for (let i = colCount - 1; i >= 0; i--) {
   let bracketColumn = document.createElement("div")
   bracketColumn.setAttribute("id", `column-${i}`)
+  bracketColumn.style.margin = "0"
   bracketColumn.style.gridTemplateRows = `repeat(${2**i}, 1fr)`
   bracketColumn.classList.add("bracket-column")
   bracketContainer.appendChild(bracketColumn)
-
   for (let x = 0; x < 2**i; x++) {
     const newBracket = bracketTemplate.cloneNode(true)
+    newBracket.querySelector(".bracket-lines")
     newBracket.removeAttribute("id")
+    newBracket.classList.add("module")
     bracketColumn.appendChild(newBracket)
   }
 }
 
-
+document.getElementById("bracket").style.display = "grid"
